da01d33302ba9eb9ae8714cc2cc2029f46b092c0
j <j@j-AO531h.(none)>
2014-03-26 18:25:59 +0000

    Creates buttons using web2py.

--- controllers/default.py
+++ controllers/default.py
@@ -70,8 +70,7 @@ def save_game_report_cal(report, budget, revenue):
     return
 
 def getDailyDevPeriod():
-    config = ConfigParser.ConfigParser()
-    config.read("applications/SplunkeGSD/application.config")
+    config=open_conf()
     return float(config.get('Development Period', 'Effort'))
 
 def getFinalRevenue(listOfTeams, revenue = None):
@@ -90,8 +89,7 @@ def getFinalRevenue(listOfTeams, revenue = None):
     return str("%.2f" % actual_revenue)
 
 def getExpectedBudget(listOfTeams):
-    config = ConfigParser.ConfigParser()
-    config.read("applications/SplunkeGSD/application.config")
+    config=open_conf()
     cost_of_dev = config.get('Developer', 'Cost_Per_Day')
     avg_developer_effort_day = getDailyDevPeriod()
     module_estimated_effort = 0
@@ -124,33 +122,36 @@ def show_saved_reports():
     return dict (title=T('Saved End of Game Reports'), result2=details)
 
 def problemSimulator():
+        config=open_conf()
 	num = random.random()
-	print num
-	config = ConfigParser.ConfigParser()
-	config.read("applications/SplunkeGSD/application.config")
 	prob = config.get('Problems', 'probability')
 	for team in session.test:
 		for mod in team.currentModules:
 			mod.hasProblem = random.random() >prob
 			print mod.hasProblem
-def view():
-    modules = []
-    statuses = {}
-    config = ConfigParser.ConfigParser()
-    config.read("applications/SplunkeGSD/application.config")
+
+def generateEndOfGameReport():
+    pass
+
+def get_locations(): 
+    config=open_conf()
     fromFile = config.items('Location')
+    locations = {}
     for loc in fromFile:
          name, pos = loc
-         name.rstrip()
-         statuses.update({name: ast.literal_eval(pos)})
+         locations[name.rstrip()] = ast.literal_eval(pos)
+    return locations
+
+def view():
+    modules = []
+    location = get_locations()
     isComplete = True
     teamEstimatesAndProgresses = [["", "Actual", "Estimated"]]
     totEstimate = 0
     totActual = 0
     for team in session.test:
          team.applyEffort()
-         statuses[team.location].append(team.getStatus())
-         modules.append((team.location , team.currentModules, team.teamSize))
+         modules.append((team.location, team.currentModules, team.teamSize, team.getStatus(), location[team.location]))
          isComplete = isComplete and team.isFinished()
          for mod in team.currentModules:
              splitLoc = team.location.split(" ")
@@ -171,53 +172,47 @@ def view():
     cost = getTotalCost(session.test, session.day)
     budgetReport = [["Cost", str("%.1f" % cost), str("%.1f" % session.budget)]];
     revenueReport = [["Revenue", str("%.1f" % float(final)), str("%.1f" % (session.revenue/2))]];
-    location = list(statuses.values())
     session.d_report = teamEstimatesAndProgresses
     session.d_budget = budgetReport
     session.d_revenue = revenueReport
     amount = str("%.2f" % ((float(final) + float(session.budget)) - float(cost)))
     final_rev =  (float(session.revenue)/2) - float(final)
-    final_cost = session.budget -cost
+    final_cost = session.budget - cost
+    print modules
     return dict(title='Team Splunke Game', saved=session.saved, amount=amount, final_rev=final_rev, final_cost=final_cost, esti = session.estimate_day, modules=modules, final=final,  cost=cost, the_revenue=session.revenue, the_budget=str("%.1f" % session.budget), locations=location, completed=complete, report=teamEstimatesAndProgresses, budget=budgetReport, revenue=revenueReport, day=session.day)
 
 def getTotalCost(listOfTeams, numDays):
-    config = ConfigParser.ConfigParser()
-    config.read("applications/SplunkeGSD/application.config")
+    config=open_conf()
     cost_of_dev = config.get('Developer', 'Cost_Per_Day')
     number_of_devs = 0
     for team in listOfTeams:
         number_of_devs = number_of_devs + team.teamSize
     return number_of_devs * float(cost_of_dev) * numDays 
 
+def open_conf():
+    config = ConfigParser.ConfigParser()
+    config.read("applications/SplunkeGSD/application.config")
+    return config
+
 def view_game():
     modules = []
     statuses = {}
-    config = ConfigParser.ConfigParser()
-    config.read("applications/SplunkeGSD/application.config")
+    config=open_conf()
     fromFile = config.items('Location')
     session.estimate_day = 0
     for loc in fromFile:
          name, pos = loc
          name.rstrip()
          statuses.update({name: ast.literal_eval(pos)})
-    isComplete = True
-    teamEstimatesAndProgresses = []
     for team in session.test:
-         statuses[team.location].append(team.getStatus())
          modules.append((team.location, team.currentModules, team.teamSize))
          for d_mod in team.currentModules:
              temp = str(d_mod).split(',')
              if int(temp[4]) >= int(session.estimate_day):
                     session.estimate_day = temp[4]
-         isComplete = isComplete and team.isFinished()
-         estimateAndProgress = []
-         for mod in team.currentModules:
-                estimateAndProgress.append([mod.name.encode("ascii"), mod.progress, mod.estimateEffort])
-         teamEstimatesAndProgresses.append([team.location, estimateAndProgress])
-    complete = "true" if isComplete else "false"
     location = list(statuses.values())
     cost = getTotalCost(session.test, session.day)
-    return dict(title='Team Splunke Game', esti = session.estimate_day, budget=str("%.1f" % session.budget), cost=cost,  the_revenue=session.revenue, modules=modules, pre=session.pre, locations=location,day=session.day, completed=complete, report=teamEstimatesAndProgresses)
+    return dict(title='Team Splunke Game', esti = session.estimate_day, completed="false", budget=str("%.1f" % session.budget), cost=cost,  the_revenue=session.revenue, modules=modules, locations=location,day=session.day)
 
 
 def config_game():
