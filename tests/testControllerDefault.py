bc0a07c79d181a9f5526d36e09d6d904cd03d065
Katharine <burtonk@tcd.ie>
2014-04-10 16:48:48 +0100

    format

--- tests/testControllerDefault.py
+++ tests/testControllerDefault.py
@@ -139,7 +139,7 @@ class TestControllerDefault(unittest.TestCase):
 		cost_of_dev = config.get('Developer', 'Cost_Per_Day')
 		self.assertEqual(cost, float(cost_of_dev))
 		
-	def testEmailQuery(self):
+	def testemailQuery(self):
 		bob = team.team(10, 'dublin', 10)
 		mod1 = module.module('TestModule', 50)
 		mod2 = module.module('TestModule2', 50)
